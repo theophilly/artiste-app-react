639938d605dc69959e3e722922f6e9e893464edc
theophilly <theophilly20@gmail.com>
2021-01-21 22:43:04 -0800

    component changes

--- src/components/ArtComponent.js
+++ src/components/ArtComponent.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./styles/ArtComponent.css";
 
 function ArtComponent({ art }) {
+  const [mouseEntered, setMouseEntered] = useState(false);
   return (
-    <div className="artImage">
+    <div
+      onMouseEnter={() => setMouseEntered(!mouseEntered)}
+      onMouseLeave={() => setMouseEntered(!mouseEntered)}
+      className="artImage"
+    >
       <img src={art.image}></img>
-      <div className="detailscontainer">
-        <p> {art.title}</p>
-        <span>{art.year}</span>
-      </div>
-      <div>
-        <p> {art.medium}</p>
-        <p> {art.dimemsion}</p>
-      </div>
+      {!mouseEntered ? (
+        <>
+          <div className="detailscontainer">
+            <p>{art.title}</p>
+            <span>{art.year}</span>
+          </div>
+          <div>
+            <p> {art.medium}</p>
+            <p> {art.dimemsion}</p>
+          </div>
+        </>
+      ) : (
+        <div className="checkFullSizeContainer">
+          <span className="checkFullSize">See Full Size</span>
+        </div>
+      )}
     </div>
   );
 }
